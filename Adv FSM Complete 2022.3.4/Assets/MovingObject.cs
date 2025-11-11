c27bd64b712c0a8059dd2dfb0f980b56bfa7c904
Chase Burdin <mariahasspoken@gmail.com>
2025-11-10 20:22:09 -0500

    fixed moving object

--- Adv FSM Complete 2022.3.4/Assets/MovingObject.cs
+++ Adv FSM Complete 2022.3.4/Assets/MovingObject.cs
@@ -12,8 +12,6 @@ public class MovingObject : MonoBehaviour
     private int i;
 
 
-
-
     // Start is called before the first frame update
     void Start()
     {
@@ -24,20 +22,20 @@ public class MovingObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
 
-        if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
+        if (Vector3.Distance(transform.position, points[i].position) < 0.02f)
         {
             i++;
 
-            if(i == points.Length)
+            if (i == points.Length)
             {
                 i = 0;
             }
 
-            
+
         }
 
-    
+
     }
 }
